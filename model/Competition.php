99505be8f9cc640d288e26eb5fc004a070c7b1fe
Harun GURBUZ <gurbuz.harun@btssio.local>
2016-09-29 11:32:28 +0200

    Création modèle compétition

---
+++ model/Competition.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Competition
+ *
+ * @author gurbuz.harun
+ */
+class Competition {
+    //put your code here
+}
